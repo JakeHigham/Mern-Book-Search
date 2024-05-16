c9e116cc0ddbe96c05a6aff105ee464f105f47c2
Jacob Higham <149442786+JakeHigham@users.noreply.github.com>
2024-05-15 21:25:55 -0500

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JakeHigham:7MHSDjIG8Dk1kJ3s@cluster0.yy9x5v6.mongodb.net/bookSearch?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://jacobhcvn65:<password>@cluster0.r5i1wo9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
