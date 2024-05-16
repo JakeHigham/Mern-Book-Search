fdfdca1c19d4eb73b23e4d93f670fbf1b659dd4c
Jacob Higham <149442786+JakeHigham@users.noreply.github.com>
2024-05-15 20:38:53 -0500

    Update connection.js
    think i fixed mongodb connection refused error

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://JakeHigham:7MHSDjIG8Dk1kJ3s@cluster0.yy9x5v6.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
